a4a6d6e85fbd29ab1cdc8733832384267b9541b5
hbm99 <ernestoblancomalmierca@gmail.com>
2022-12-03 13:04:52 -0500

    Add node_description to Content (pending setting in Gephi nodes view), add Angela's notes contents (pending other contents from bibliography)

--- content.py
+++ content.py
@@ -1,7 +1,7 @@
 from typing import List, Dict
 
 class Content:
-    def __init__(self, *name) -> None:
+    def __init__(self, *name, node_description = '') -> None:
         self.name = name
         self.distances : Dict[Content, List[int]] = {}
         
--- main.py
+++ main.py
@@ -8,10 +8,53 @@ if __name__ == '__main__':
     
     pdfs_text_info = pdfs_info_dict("pdfs/")
     
-    plu_content = Content('plu')
-    epsilon_content = Content('epsilon')
-    maquina_content = Content('maquina')
-    contents = [epsilon_content, plu_content, maquina_content, Content('conferencia'), Content('profesor')]
+    contents = [
+        
+        #region Angela's notes contents
+        
+        #region Interpolación
+        
+        Content('formula', 'lagrange', node_description='Fórmula de Lagrange'),
+        Content('formula', 'interpolacion', 'newton', node_description='Fórmula de interpolación de Newton'),
+        Content('hermite', node_description='Forma de Hermite del polinomio de interpolación'),
+        Content('interpolacion', 'tramos', node_description='Interpolación por tramos'),
+        
+        #endregion
+        
+        #region Aplicaciones de la interpolación
+        
+        Content('diferenciacion', 'numerica', node_description='Diferenciación numérica'),
+        Content('integracion', 'aproximada', node_description='Integración aproximada'),
+        Content('newton', 'cotes', node_description='Fórmulas de Newton Cotes'),
+        Content('extrapolacion', 'richardson', node_description='Extrapolación de Richardson'),
+        Content('romberg', node_description='Algoritmo de Romberg'),
+        
+        #endregion
+        
+        #region Ecuaciones diferenciales ordinarias
+        
+        Content('integracion', 'serie', 'Taylor', node_description='Integración por serie de Taylor'),
+        Content('metodo', 'euler', node_description='Método de Euler'),
+        Content('runge-kutta', node_description='Métodos de Runge-Kutta'),
+        
+        #endregion
+        
+        #region Aproximación de funciones por mínimos cuadrados
+        
+        Content('ajuste', 'curva', 'lineal', node_description='Ajuste de curvas lineal'),
+        Content('aproximacion', 'lineal', 'multiple', node_description='Aproximación lineal múltiple'),
+        
+        #endregion
+        
+        #endregion
+        
+        #region "Resumen de las clases de Numérica"'s contents
+        
+        
+        
+        #endregion
+        
+    ]
     
     graph = Graph(pdfs_text_info, contents)
     
